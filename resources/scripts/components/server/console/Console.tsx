eb9cf05cae2fb2e8d5807ad861fbcdd564e3c5da
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2023-07-26 23:44:03 +0530

    Update Console.tsx

--- resources/scripts/components/server/console/Console.tsx
+++ resources/scripts/components/server/console/Console.tsx
@@ -20,7 +20,7 @@ import 'xterm/css/xterm.css';
 import styles from './style.module.css';
 
 const theme = {
-  background: '#111F26',
+  background: '#0a001a',
   cursor: 'transparent',
   black: th`colors.black`.toString(),
   red: '#E54B4B',
