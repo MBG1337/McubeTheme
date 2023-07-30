1d39aac53ced09314dc95dcad198d38236d20879
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2023-07-30 06:18:24 +0530

    Add files via upload

---
+++ resources/scripts/routers/ServerToolsBar.tsx
@@ -0,0 +1,86 @@
+import TransferListener from '@/components/server/TransferListener';
+import React, { useEffect, useState } from 'react';
+import { NavLink, Route, Switch, useRouteMatch } from 'react-router-dom';
+import NavigationBar from '@/components/NavigationBar';
+import TransitionRouter from '@/TransitionRouter';
+import WebsocketHandler from '@/components/server/WebsocketHandler';
+import { ServerContext } from '@/state/server';
+import { CSSTransition } from 'react-transition-group';
+import Can from '@/components/elements/Can';
+import Spinner from '@/components/elements/Spinner';
+import { NotFound, ServerError } from '@/components/elements/ScreenBlock';
+import { httpErrorToHuman } from '@/api/http';
+import { useStoreState } from 'easy-peasy';
+import SubNavigation from '@/components/elements/SubNavigation';
+import InstallListener from '@/components/server/InstallListener';
+import ErrorBoundary from '@/components/elements/ErrorBoundary';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { useLocation } from 'react-router';
+import ConflictStateRenderer from '@/components/server/ConflictStateRenderer';
+import PermissionRoute from '@/components/elements/PermissionRoute';
+import routes from '@/routers/routes';
+import tw from 'twin.macro';
+
+export default () => {
+    const match = useRouteMatch<{ id: string }>();
+    const location = useLocation();
+
+    const rootAdmin = useStoreState((state) => state.user.data!.rootAdmin);
+    const [error, setError] = useState('');
+
+    const id = ServerContext.useStoreState((state) => state.server.data?.id);
+    const uuid = ServerContext.useStoreState((state) => state.server.data?.uuid);
+    const inConflictState = ServerContext.useStoreState((state) => state.server.inConflictState);
+    const serverId = ServerContext.useStoreState((state) => state.server.data?.internalId);
+    const getServer = ServerContext.useStoreActions((actions) => actions.server.getServer);
+    const clearServerState = ServerContext.useStoreActions((actions) => actions.clearServerState);
+    const eggid = ServerContext.useStoreState(state => state.server.data?.eggid);
+
+    const to = (value: string, url = false) => {
+        if (value === '/') {
+            return url ? match.url : match.path;
+        }
+        return `${(url ? match.url : match.path).replace(/\/*$/, '')}/${value.replace(/^\/+/, '')}`;
+    };
+
+    useEffect(
+        () => () => {
+            clearServerState();
+        },
+        []
+    );
+
+    useEffect(() => {
+        setError('');
+
+        getServer(match.params.id).catch((error) => {
+            console.error(error);
+            setError(httpErrorToHuman(error));
+        });
+
+        return () => {
+            clearServerState();
+        };
+    }, [match.params.id]);
+
+    return (
+        <>
+        {eggid === 4 &&
+        <><span css={tw`select-none text-base hidden pl-4 sm:block text-accent-50 font-bold`}>Tools</span>
+        
+        <span className='ml-4 text-white font-bold text-base py-2'>
+        <a href={'https://de1.mrc4t.xyz/csgo/demos/' + uuid} rel="noreferrer" target={'_blank'}>
+                    <FontAwesomeIcon className='text-accent-50' icon={faExternalLinkAlt} /> GOTVDemos
+                </a></span></>
+                                            }
+                                            {eggid === 4 &&
+                                            <span className='ml-4 text-white font-bold text-base mb-4 '>
+                                            <a href={'https://de2.mrc4t.xyz/csgo/demos/' + uuid} rel="noreferrer" target={'_blank'}>
+                                                <FontAwesomeIcon className='text-accent-50' icon={faExternalLinkAlt}/> GOTVDemos
+                                            </a>
+                                            </span>
+                                            }
+        </>
+    );
+};
